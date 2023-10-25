40dd47001eb4e32d24c45d93edd5ead38fd3e776
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2023-10-25 21:18:19 +0200

    hecho hasta el 5, del 6 no consigo hacer los filtros con los formatos date

--- modelo/urls.py
+++ modelo/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('proyecto/proyecto',views.listar_proyectos,name='lista_proyectos'),
     path('proyecto/proyecto/<int:id_proyecto>/',views.tarea_proyecto,name='tarea_proyecto'),
     path('tarea/tarea/<int:id_tarea>/',views.usuario_tarea,name='usuario_tarea'),
-    #tareas con texto x en observacion durante la asignacion
-    path('tarea/tarea/<str:texto_observacion>/',views.texto_tarea_asignacion,name='observacion_asignacion')
+    path('tarea/tarea/<str:texto_observacion>/',views.texto_tarea_asignacion,name='observacion_asignacion'),
+    #Crear una URL que muestre todos los proyectos que se han creado entre dos fechas y el estado sea “Completada”.
+    #path('proyecto/proyecto/<int:anho_init>/<int:mes_init>/<int:dia_init>/<int:anho_fin>/<int:mes_fin>/<int:dia_fin>/',views.proyectos_rango,#name='proyectos_rango') no funciona
+
+    #Crear una URL que muestre todos las tareas que se han creado entre dos años y el estado sea “Completada”
+    path('proyecto/proyecto/<int:anho_init>/<int:anho_fin>/',views.tareas_anhos,name='tareas_2anhos')
 ]
--- modelo/views.py
+++ modelo/views.py
@@ -25,3 +25,11 @@ def texto_tarea_asignacion(request, texto_observacion):
     tareas=Asignacion_de_tarea.objects.select_related('tarea')
     tareas=tareas.filter(Observaciones__contains=texto_observacion)
     return render(request,'tarea/tarea.html',{'observacion_tarea':tareas})
+
+#def proyectos_rango(request,anho_init,mes_init_dia_init,anho_fin,mes_fin,dia_fin):
+#    proyecto=Proyecto.objects.prefetch_related('colaboradores').select_related('creador')
+#    proyecto=proyecto.filter(Q(Fecha_de_Inicio__year<=anho_init)|Q(Fecha_de_Inicio__year=anho_init & Fecha_de_Inicio__month<mes_init)|Q#(Fecha_de_Inicio__year=anho_init & Fecha_de_Inicio__month=mes_init)) no soy capaz de hacer funcionar los or 
+
+def tareas_anhos(request,anho_init,anho_fin):
+    tareas=Tarea.objects.select_related('proyecto')
+    #tareas=tareas.filter(Fecha_de_Creación__year<anho_init)
